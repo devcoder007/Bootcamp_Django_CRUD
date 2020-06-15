a9c6f5584c6df3765d9a5db8c03bcdc3a9961b96
devcoder007 <41121180+devcoder007@users.noreply.github.com>
2020-06-15 13:08:21 -0400

    Update settings.py

--- postmann/settings.py
+++ postmann/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'postmann.wsgi.application'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kampuskonnect.kk@gmail.com'
 DEFAULT_FROM_EMAIL = 'kampuskonnect.kk@gmail.com'
-EMAIL_HOST_PASSWORD = 'Rish@1996'
+EMAIL_HOST_PASSWORD = 'your_pass'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
